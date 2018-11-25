8ee9c7fb60d304bc542d660c1c9d161399afa641
Dylan Black <dylan.black2021@gmail.com>
2018-11-25 18:57:18 -0500

    Added color functionality

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ var Command = {
 function setup() {
     angleMode(DEGREES);
     createCanvas(400, 400);
-    stroke(255, 0, 255);
+    stroke(255, 255, 255);
     background(0);
 
     //create buttons and get textbox by id
@@ -57,20 +57,17 @@ function setup() {
 
         //loop through tokens, find commands and run correlated functions
         for(var i = 0; i < tokens.length; i++) {
-            try {
-                stroke(hue, sat, bright);
-                if(tokens[i].includes("[")) {
-                    let parameters = tokens[i].match(/\[.*?\]/)[1] //what's inside of [] 
-
-                    console.log(parameters) //Find what's inside of []
-                } else {
-                    Command[tokens[i]](tokens[++i]);
-                }
-            //if we can't find the function we throw an error
-            } catch (error) {
-                alert(tokens[i] + " is not a valid function");
-            }
-            
+            console.log(tokens[i]);
+            if(tokens[i].includes("[")) {
+                let params = getParams(tokens[i]);
+                console.table(params);
+                let command = getCommand(tokens[i]);
+                console.log(command);
+                Command[command](params[0], params[1], params[2]);
+                // console.log(parameters) //Find what's inside of []
+            } else {
+                Command[tokens[i]](tokens[++i]);
+            }  
         }
     });
     
@@ -79,7 +76,20 @@ function setup() {
         background(0);
     });
 }
-  
+
+function getParams(token) {
+    let openBrack = token.indexOf("["); 
+    let closeBrack = token.indexOf("]");
+    let parameters = token.substr(openBrack+1,closeBrack-openBrack-1);
+
+    return parameters.split(",")
+}
+
+function getCommand(token) {
+    let openBrack = token.indexOf("[");
+    return token.substr(0, openBrack);  
+}
+
 function draw() {
     
 }
