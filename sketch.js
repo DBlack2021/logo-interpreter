6a4321ab971185a6b7899bf3a3da638c3fe232d1
Dylan Black <dylan.black2021@gmail.com>
2018-11-25 14:50:35 -0500

    Added run and clear buttons

--- sketch.js
+++ sketch.js
@@ -40,20 +40,31 @@ var Command = {
 function setup() {
     angleMode(DEGREES);
     createCanvas(400, 400);
-    translate(width/2, height/2);
+    
     background(0);
+    runB = createButton('Run');
+    clearB = createButton('Clear');
     textbox = select('#turtle-text')
 
-    let text = textbox.value();
-    let tokens = text.split(' ');
+    runB.mousePressed(function() {
+        translate(width/2, height/2);
+        let text = textbox.value();
+        let tokens = text.split(' ');
+    
+        pendown = true;
 
-    pendown = true;
+        console.table(tokens);
+        
+        //loop through tokens, find commands and run correlated functions
+        for(var i = 0; i < tokens.length; i++) {
+            Command[tokens[i]](tokens[++i]);
+        }
+    });
     
-    //loop through tokens, find commands and run correlated functions
-    for(var i = 0; i < tokens.length; i++) {
-        Command[tokens[i]](tokens[++i]);
-    }
-    
+    clearB.mousePressed(function() {
+        background(0);
+    });
+
     //TODO: Add run button and pu/pd
     /*
     More complicated command ideas:
