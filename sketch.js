3e272d252b15d493430fda9eac5184314eb24e3d
Dylan Black <dylan.black2021@gmail.com>
2018-11-25 15:35:30 -0500

    Error handling and began color functionality

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
 let textbox;
+let hue, sat, bright = 100;
 var pendown = true; 
 
 var Command = {
     fd: function (dist) {
         if(pendown) {
-            stroke(255);
-            strokeWeight(5);
             line(0, 0, dist, 0)
         }
         translate(dist, 0);
@@ -13,8 +12,6 @@ var Command = {
 
     bd: function (dist) {
         if(pendown) {
-            stroke(255);
-            strokeWeight(5);
             line(0, 0, -dist, 0)
         }
         translate(-dist, 0);
@@ -28,39 +25,56 @@ var Command = {
         rotate(-dist)
     },
 
-    pu: function(dist) {
-        pendown = false;
+    //TODO: Combine hu, st, br into one big clr function where the syntax is clr[h,s,b] (NO SPACES)
+    hu: function(col) {
+        hue = col;
     },
 
-    pd: function(dist) {
-        pendown = true;
+    st: function(col) {
+        sat = col;
+    },
+
+    br: function(col) {
+        bright = col;
     }
 };
 
 function setup() {
     angleMode(DEGREES);
+    colorMode(HSB);
     createCanvas(400, 400);
-    
+    stroke(255, 0, 255);
     background(0);
+
+    //create buttons and get textbox by id
     runB = createButton('Run');
     clearB = createButton('Clear');
     textbox = select('#turtle-text')
 
+    //when run button is pressed
     runB.mousePressed(function() {
+        background(0);
         translate(width/2, height/2);
         let text = textbox.value();
         let tokens = text.split(' ');
     
         pendown = true;
 
-        console.table(tokens);
-        
+        console.log(tokens);
+
         //loop through tokens, find commands and run correlated functions
         for(var i = 0; i < tokens.length; i++) {
-            Command[tokens[i]](tokens[++i]);
+            try {
+                stroke(hue, sat, bright);
+                Command[tokens[i]](tokens[++i]);
+            } catch (error) {
+                alert(tokens[i] + " is not a valid function");
+            }
+            
         }
     });
     
+    //when clear button is pressed
     clearB.mousePressed(function() {
         background(0);
     });
